e1f8eba50a3f2571c78583f6cd7f26cbf2486f49
felexkemboi <felokemboi10@gmail.com>
2018-05-09 13:20:19 +0300

    added directory to enable collection of static files

--- musicstore/settings.py
+++ musicstore/settings.py
@@ -152,11 +152,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static")),
-
-
+"""
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+ 
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
 
 """
 SOCIALACCOUNT_QUERY_EMAIL = True
