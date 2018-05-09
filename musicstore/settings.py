3427c35b35fcca447f03178477eb6697b84eea67
felexkemboi <felokemboi10@gmail.com>
2018-05-09 13:30:28 +0300

    mmm

--- musicstore/settings.py
+++ musicstore/settings.py
@@ -156,11 +156,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static")),
 """
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
  
 STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
+os.path.join(BASE_DIR, 'musicstore/static'),
 )
 
 """
